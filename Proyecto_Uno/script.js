6dcb5698f7d91e82b7e90b761d3e9d679775bedf
Kevin <kevinlibardorosas@gmail.com>
2026-02-21 11:31:12 -0500

    implementacion de proyecto html css y js

---
+++ Proyecto_Uno/script.js
@@ -0,0 +1,15 @@
+const btn = document.getElementById(themeBtn);
+
+btn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+
+    if(document.body.classList.contains("dark")){
+        btn.textContent = "Modo Claro"
+    }
+    else{
+        btn.textContent = "Modo Oscuro"
+    }
+
+}
+
+)
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # DAWYSO-A
 repositorio publico para alojar los proyectos de clase de la asignatura.
+
+## Proyecto 1
+Proyecto de repaso de conceptos de HTML, CSS, JS
+
